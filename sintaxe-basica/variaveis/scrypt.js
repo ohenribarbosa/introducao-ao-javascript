2a5c9b3be09316bb76ee5400b392742c06681898
ohenribarbosa <ohenribarbosa@gmail.com>
2022-04-21 14:19:02 -0300

    var e let

---
+++ sintaxe-basica/variaveis/scrypt.js
@@ -0,0 +1,13 @@
+// var, let | camelCase, hoisting, escopo, redeclaração e reatribuição
+/* var firstName = "João"
+var lastName = "Silva"
+
+if(firstName === "João") {
+  var firstName = "Pedro"
+  let lastName = "Souza"
+
+  console.log("if-block: ", firstName, lastName)
+}
+
+console.log(firstName, lastName) */
+
